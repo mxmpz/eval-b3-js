6fe0badfc8a5bf622f732136b3365e582e6d86ef
Maxime Prouzat <maximeprouzat6@gmail.com>
2023-05-08 16:06:10 +0200

    createKennel

--- backend/src/controllers/kennel.controllers.js
+++ backend/src/controllers/kennel.controllers.js
@@ -1,1 +1,32 @@
 const Kennel = require('../data/models/Kennel')
+
+// Fonction pour créer un chenil
+const createKennel = async (kennel) => {
+
+    // On créer un chenil en respectant le schéma
+   const _kennel = new Kennel({
+    name: kennel.name,
+    description: kennel.description,
+    adress: {
+      street: kennel.street,
+      number: kennel.number,
+      postcode: kennel.postcode,
+      city: kennel.city,
+      country: kennel.country
+    },
+    dogs: kennel.dogs
+  })
+
+  // On sauvegarde et enregistre le chenil
+  const savedKennel = await _kennel.save()
+
+  // On transforme le résultat en objet
+  const savedKennelObject = savedKennel.toObject()
+
+  return savedKennelObject
+}
+
+// On exporte les fonctions
+module.exports = {
+  createKennel
+}
