9db8d2f9c50c70cf20818cc639c24f89f0381dea
Maxime Prouzat <maximeprouzat6@gmail.com>
2023-05-08 17:25:12 +0200

    creation routes users ( / ) + branchement

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,3 +11,14 @@ connect()
 // Paramétrage de Express pour le body et le JSON
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+
+// On branche les route users sur le fichier correspondant, le nom index.js est utilisé par défaut.
+app.use('/users', require('./routes/users'))
+
+app.get('/', (req, res) => {
+  res.send('Hello world Express !')
+})
+
+app.listen(port, () => {
+  console.log('Server is running on port ' + port)
+})
---
+++ backend/src/routes/users/index.js
@@ -0,0 +1,23 @@
+const { getUsers, createUser } = require('../../controllers/users.controller')
+const router = require('express').Router()
+
+// Methode pouvant être utilisé par défaut
+router.route('/')
+  // Afficher les utilisateurs
+  .get(async (req, res) => {
+    const users = await getUsers()
+    return res.send(users)
+  })
+
+  // Créer un utilisateur
+  .post(async (req, res) => {
+    try {
+      // Appel la méthode du contrôleur
+      const userCreated = await createUser(req.body)
+      return res.send(userCreated)
+    } catch (error) {
+      // Si il y a une erreur, on renvoit une erreur 500 + detail dans la réponse
+      console.error(error)
+      return res.status(500).send(error)
+    }
+  })
